18feaaec683b7b0e94564d697ca05094ba56d5d4
tmreign <tomwanyoike9@gmail.com>
2019-06-17 15:28:31 +0300

    styling the documets

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,12 @@ function Contact(first, last,email) {
     $("input.new-city").val("");
     $("input.new-county").val("");
   }
+
   
   
   
   
-  
-  $(document).ready(function() {
+  $(document).ready(function(){
   
     $("#add-address").click(function() {
       $("#new-addresses").append('<div class="new-address">' +
@@ -138,4 +138,170 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     
     $(target).fadeIn(600);
     
+  })
+
+//   var finalCost = [];
+
+
+//   function item(size,crust){
+//     this.size= size;
+//     this.crust=crust;
+//     this.price = 0;
+
+//   }
+
+//   var pizaaSize =['large','medium', 'small'];
+//   var pizaaCrust =['crisy','stuffed', 'glutted'];
+
+
+//   item.prototype.costofpizza =function(){
+
+//     if (this.size == pizaaSize[0]){
+//       this.price +=1200;
+//     }
+
+//     else if(this.size == pizaaSize[1]){
+//       this.price += 950;
+//     }
+//     else if(this.size == pizaaSize[2]){
+//       this.price += 550;
+//     }
+   
+//     if(this.crust == pizaaCrust[0]){
+//       this.price +=150;
+//     }
+
+//     else if(this.crust == pizaaCrust[1]){
+//       this.price +=100;
+//     }
+//     else if(this.crust == pizaaCrust[2]){
+//       this.price +=100;
+//     }
+
+
+//   };
+// // return this.price;
+
+// item.prototype.totalCost = function() {
+//   //created a variable shoppingCartTotal and initialized it to zero
+//   var shoppingCartTotal = 0;
+//   //this for loop iterates through the total order(s) to determine the number of order(s)
+//   for (var order = 0; order < finalCost.length; order++) {
+//     //shoppingCartTotal is now assigned a new value after the order(s) is added
+//     shoppingCartTotal += totalOrderPrice[order];
+//   }
+//   return shoppingCartTotal;
+// }
+
+// $(document).ready(function() {
+//   $("form#customized-pizza").submit(function(event) {
+//     event.preventDefault();
+//     var customSize = $("select#size").val();
+//     var customcrust = $("select#crust").val();
+//     var customtopping = $("input#topping").val();
+//     var newPizzaOrder = new item(customSize, customcrust, customtopping);
+//     newPizzaOrder.costOfPizza();
+//     finalCost.push(newPizzaOrder.price);
+//     $("#pizza-size").show();
+//     $("#pizza-crust").show();
+//     $("#pizza-topping").show();
+//     $("#pizza-size").append("\t" + "\t" + customSize);
+//     $("#pizza-crust").append("\t" + "\t" + customcrust);
+//     $("#pizza-topping").append("\t" + "\t" + customtopping);
+//     $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCost());
+//   });
+// });
+
+var totalOrderPrice = [];
+//------Constructor Placeorder created with it's properties----//
+function Placeorder(size, crust, topping) {
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.price = 0;
+  // this.deliveryFee = 200;
+}
+//--Arrays to declare the pizzaSize,pizzaCrust and pizzaTopping to be used in the prototype CostOfPizza--//
+var pizzaSize = ["Small", "Medium", "Large"];
+var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
+var pizzaTopping = ["Bacon", "Chicken", "Beef", "Vegetables"];
+//-Prototype CostOfPizza created to determine the price based on the three properties i.e size,cheese,topping-//
+Placeorder.prototype.costOfPizza = function() {
+  if (this.size === pizzaSize[0]) {
+    this.price += 300;
+  } else if (this.size === pizzaSize[1]) {
+    this.price += 400;
+  } else if (this.size === pizzaSize[2]) {
+    this.price += 500;
+  }
+  if (this.crust === pizzaCrust[0]) {
+    this.price += 100;
+  } else if (this.cheese === pizzaCrust[1]) {
+    this.price += 200;
+  } else if (this.cheese === pizzaCrust[2]) {
+    this.price += 300;
+  }
+  if (this.topping === pizzaTopping[0]) {
+    this.price += 100;
+  } else if (this.topping === pizzaTopping[1]) {
+    this.price += 200;
+  } else if (this.topping === pizzaTopping[2]) {
+    this.price += 300;
+  } else if (this.topping === pizzaTopping[3]) {
+    this.price += 50;
+  }
+  return this.price;
+}
+//----End Of the Prototype CostOfPizza---//
+
+Placeorder.prototype.costOfDelivery = function() {
+  return this.deliveryFee;
+}
+
+//---Creation of prototype totalCost to be used to compute the total cost of all the orders--//
+Placeorder.prototype.totalCost = function() {
+  //created a variable shoppingCartTotal and initialized it to zero
+  var shoppingCartTotal = 0;
+  //this for loop iterates through the total order(s) to determine the number of order(s)
+  for (var order = 0; order < totalOrderPrice.length; order++) {
+    //shoppingCartTotal is now assigned a new value after the order(s) is added
+    shoppingCartTotal += totalOrderPrice[order];
+  }
+  return shoppingCartTotal;
+}
+//----End Of the Prototype totalCost---//
+
+//---Created and object called ShippingAddress---//
+function ShippingAddress(cityName, cityAvenueName, cityStreetName, nameOfBuilding, phoneNumber) {
+  this.cityName = cityName;
+  this.cityAvenueName = cityAvenueName;
+  this.CityStreetName = cityStreetName;
+  this.nameOfBuilding = nameOfBuilding;
+  this.phoneNumber = phoneNumber;
+  this.deliveryAddress = (cityStreetName + " : \n" + cityAvenueName + " : \n" + nameOfBuilding + " : \n");
+}
+//----End Of the Constructor ShippingAddress---//
+
+//--------------------------- END OF BUSINESS LOGIC DESIGNED AND TESTED------------------------//
+//-------------------------------------USER INTERFACE LOGIC------------------------------------//
+$(document).ready(function() {
+  $("form#customized-pizza").submit(function(event) {
+    event.preventDefault();
+    var customSize = $("select#size").val();
+    var customcrust = $("select#crust").val();
+    var customtopping = $("input#topping").val();
+    var newPizzaOrder = new Placeorder(customSize, customcrust, customtopping);
+    newPizzaOrder.costOfPizza();
+    totalOrderPrice.push(newPizzaOrder.price);
+    $("#pizza-size").show();
+    $("#pizza-crust").show();
+    $("#pizza-topping").show();
+    $("#pizza-size").append("\t" + "\t" + customSize);
+    $("#pizza-crust").append("\t" + "\t" + customcrust);
+    $("#pizza-topping").append("\t" + "\t" + customtopping);
+    $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCost());
   });
+  $("#checkout-btn").click(function() {
+    location.reload();
+  });
+});
